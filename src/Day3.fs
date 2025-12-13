d48d99769d78122cb6ce01b5e702d731c4334716
aerlingsson <arnar.h.erlingsson@gmail.com>
2025-12-13 22:33:31 +0100

    Day3 part2

--- src/Day3.fs
+++ src/Day3.fs
@@ -12,14 +12,29 @@ module Day3 =
             >> Array.indexed
         )
 
+    let rec private chooseBatteries (chosenBatteries: (int * int) array) (chooseCount: int) (bank: (int * int) array) =
+        if chosenBatteries.Length = chooseCount then
+            chosenBatteries
+        else
+            let leftToChoose = chooseCount - chosenBatteries.Length
+            let takeFirst = (bank.Length - leftToChoose) + 1
+
+            let best = bank |> Array.take takeFirst |> Array.maxBy snd
+
+            let remainingBank = bank |> Array.filter (fun (idx, _) -> idx > fst best)
+            chooseBatteries (Array.append chosenBatteries [| best |]) chooseCount remainingBank
+
+    let private batteriesToInt batteries =
+        batteries
+        |> Array.map (snd >> string)
+        |> String.concat String.Empty
+        |> Int64.Parse
+
+    let combineBatteries chosenBatteries chooseCount =
+        chooseBatteries chosenBatteries chooseCount >> batteriesToInt
+
     let part1 (input: string) =
-        input
-        |> parse
-        |> Array.map (fun bank ->
-            let best = bank |> Array.take (bank.Length - 1) |> Array.maxBy snd
+        input |> parse |> Array.map (combineBatteries [||] 2) |> Array.sum
 
-            let secondBest =
-                bank |> Array.filter (fun (idx, _) -> idx > fst best) |> Array.maxBy snd
-
-            Int32.Parse $"{snd best}{snd secondBest}")
-        |> Array.sum
+    let part2 (input: string) =
+        input |> parse |> Array.map (combineBatteries [||] 12) |> Array.sum
